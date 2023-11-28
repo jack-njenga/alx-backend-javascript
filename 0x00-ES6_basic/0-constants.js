283891469234899b1008923e0ba1fbfc3e33373b
jack-njenga <jacknjenga99@gmail.com>
2023-11-28 13:26:08 +0300

    .

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,8 @@
+export function taskFirst () {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+export function taskNext () {
+  const combination = 'But sometimes let is okay';
+  return combination;
+}
